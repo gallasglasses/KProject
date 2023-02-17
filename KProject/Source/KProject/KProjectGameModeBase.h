8b3b1cfb446f549fbf32228d0485ed1249a415c5
Galla Syrodoeva <55047006+gallasglasses@users.noreply.github.com>
2023-02-17 14:35:14 +0100

    Init commit
    Create project

---
+++ KProject/Source/KProject/KProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KPROJECT_API AKProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
